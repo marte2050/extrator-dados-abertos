cc63c24684fbd7a5f9b5a8b2664c99bf959c1990
Jonathan W <150624002+marte2050@users.noreply.github.com>
2023-11-23 17:16:52 -0300

    Corrige erro no nome do pipeline.

--- extrator_dados_abertos/crawling/settings.py
+++ extrator_dados_abertos/crawling/settings.py
@@ -6,5 +6,5 @@ REQUEST_FINGERPRINTER_IMPLEMENTATION = '2.7'
 TWISTED_REACTOR = 'twisted.internet.asyncioreactor.AsyncioSelectorReactor'
 FEED_EXPORT_ENCODING = 'utf-8'
 LOG_ENABLED = True
-ITEM_PIPELINES = {'crawling.pipeline.vagas_pipeline.VagasPipeline': 1}
+ITEM_PIPELINES = {'crawling.pipelines.vagas_pipeline.VagasPipeline': 1}
 FILES_STORE = 'data/'
